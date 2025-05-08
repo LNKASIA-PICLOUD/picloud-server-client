7e8d0de1db0948450c6dafecc358afd9819bdf76
dhaval-lnkasia <dhaval@lnkasia.com>
2025-05-08 12:43:14 +0000

    main.cpp

--- test/manual/windows close event/main.cpp
+++ test/manual/windows close event/main.cpp
@@ -81,7 +81,7 @@ int main()
     int argc;
     wchar_t **argv = CommandLineToArgvW(commandLine, &argc);
 
-    const auto process = getHWND(argc == 2 ? argv[1] : L"owncloud.exe");
+    const auto process = getHWND(argc == 2 ? argv[1] : L"picloud.exe");
 //    const auto process = getHWND(L"WINDOW_TEST.exe");
 
     std::wcout << "Query: WM_QUERYENDSESSION" << std::endl;
